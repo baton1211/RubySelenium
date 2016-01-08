2737b9dc6bf69efda7bd53536a0928f585090d49
unknown <dat.nguyen@LGDN14041-W7D01.logigear.com>
2016-01-08 16:24:50 +0700

    commit dat2

--- CapybaraRuby/pages/home_page.rb
+++ CapybaraRuby/pages/home_page.rb
@@ -10,5 +10,6 @@ class HomePage < CommonPage
   end
 
   element :lnk_signin, '.dropdown-menu>li>a[href*="signin"]'
+  element :lnk_sign1in, '.dropdown-menu>li>a[href*="signin"]'
   element :lnk_users, :xpath, "//a[contains(text(),'Users')]"
 end
